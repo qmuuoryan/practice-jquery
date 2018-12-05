11fd469f5c38d5152a0d57351ff87f1a2278bf67
papi <ryanmuuo91@gmail.com>
2018-12-05 12:04:10 +0300

    add changes

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+jquery(jquery).ready(function() {
+      Jquery("h1").click(function() {
+        alert("this is a header.");
+      });
+
+      jquery("p").click(function() {
+        alert("This is a paragraph.");
+      });
+
+      jquery("img").click(function() {
+        alert("this is an image.");
+      });
+    });
